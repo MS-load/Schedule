88c7031f62de30a93b757bbddfb8631deeea14b7
Mamta Srinivas <mamtasrinivassp@gmail.com>
2019-11-27 21:39:45 +0100

    Enabled LS data on reload

--- Scripts/todo.js
+++ Scripts/todo.js
@@ -1,6 +1,22 @@
 $(document).ready(function () {
     addEventListener()
+    restoreTaskList()
 })
+
+
+
+function restoreTaskList() {
+    let data = localStorage.getItem("task")
+    if (data) {
+        let restoreList = JSON.parse(data)
+        console.log(restoreList)
+        for (var i = 0; i < (restoreList.length); i++) {
+            $("ul").append("<li>" + restoreList[i] + "</li>")
+        }
+    } else {
+        restoreList = []
+    }
+}
 
 function addEventListener() {
     $("button").click(addTask)
@@ -10,22 +26,40 @@ function addTask() {
     let task = $("#input").val()
     $("ul").append("<li>" + task + "</li>")
 
-    
     const listOfTasks = []
-    $("ul li").each(function() { listOfTasks.push($(this).text())})
+    $("ul li").each(function () { listOfTasks.push($(this).text()) })
 
     console.log(listOfTasks)
     saveTaskToLS(listOfTasks)
+
+    addPrefix()
+    addSuffix()
+
+    $("#input").val("")
+
+}
+
+function addPrefix() {
+
+}
+
+function addSuffix() {
+
+}
+
+function removeTask() {
     getTaskFromLS()
 }
 
 
-
-
-function saveTaskToLS(task){
+function saveTaskToLS(task) {
     localStorage.setItem('task', JSON.stringify(task))
 }
 
 function getTaskFromLS() {
-    return console.log(JSON.parse(localStorage.getItem('listOfTasks')) || [])
+    return console.log(JSON.parse(localStorage.getItem('task')) || [])
+}
+
+function clearTaskFromLS() {
+    localStorage.clear()
 }
