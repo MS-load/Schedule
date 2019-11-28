fd8bfaacc546c0f370e0fb958f470ec27d7c313e
Lisa <lisa.bengtsson@live.com>
2019-11-28 15:33:16 +0100

    Calendar working with js

--- Scripts/calendar.js
+++ Scripts/calendar.js
@@ -1,129 +1,18 @@
-window.addEventListener('load', loadPage)
 
-function loadPage() {
-getMonthFromApi()
+
+const currentDate = new Date()
+document.getElementById("calendar").innerHTML = currentDate.getMonth() + 1;
+let numberOfDaysInMonth = new Date(currentDate.getFullYear(), currentDate.getMonth(), 0).getDate()
+
+console.log(numberOfDaysInMonth)
+
+let dateContainer = document.getElementById("dateContainer")
+
+for (let i = 1; i <= numberOfDaysInMonth; i++) {
+    console.log("add box")
+    let dateBox = document.createElement("div")
+    dateBox.innerText = i
+    dateContainer.appendChild(dateBox)
 }
-
-
-
-
-function getMonthFromApi() {
-$.ajax({
-    url: "http://api.dryg.net/dagar/v2.1/2019/12?callback=dinmetod",
-    type: "GET",
-    dataType: "jsonp",
-    complete: function(response) {
-        const month = response.responseJSON.dagar
-
-        for (const day of month) {
-            if (day.helgdag) {
-                console.log(day.datum)
-            }
-        }
-    }
-
-})
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* window.addEventListener('load', loadPage) 
-
-
-function loadPage () {
-    displayCalendar()
-} */
-/* 
-let body = document.getElementsByTagName('body')[0];
-
-today = new Date();
-currentMonth = today.getMonth();
-currentYear = today.getFullYear();
-selectYear = document.getElementById('year')
-selectMonth = document.getElementById('month')
-
-months = {
-    0: 'Dec',
-    1: 'Jan'
-};
-
-
-
-monthAndYear = document.getElementById('displaymonthandyear');
-displayCalendar(currentMonth, currentYear);
-
-
-
-
-
-
-
-
-function displayCalendar(month, year) {
-
-
-    let firstDay = (new Date(year, month)).getDay();
-
-    let date = 1;
-
-    table = document.getElementById('calendar-full-body');
-
-    table.innerHTML = '';
-    monthAndYear.innerHTML = months[month] + '' + year;
-    selectYear.value = year;
-    selectMonth.value = month;
-    
-    
-    
-    for (let i = 0; i < 6; i++) {
-
-    let row = document.createElement('tr');
-
-
-    for (let j = 0; j < 7; j++) {
-        if (i=== 0 && j < firstDay) {
-            cell = document.createElement('td');
-            cellText = document.createTextNode('');
-            cell.appendChild(cellText);
-            row.appendChild(cell);
-        }
-
-        else if (date > daysInMonth(month, year)) {
-            break;
-        }
-
-        else {
-            cell = document.createElement('td');
-            cellText = document.createTextNode(date);
-            cell.appendChild(cellText);
-            row.appendChild(cell);
-            date++;
-
-        }
-    }
-
-    table.appendChild(row);
-}
-
-function daysInMonth(iMonth, iYear) {
-    return 32 - new Date(iYear, iMonth, 32).getDate();
-}
-
-} */
 
 
