f2079f74c88c83766fbbd8ecacb2b04df5e26259
Lisa <lisa.bengtsson@live.com>
2019-11-29 14:34:55 +0100

    Made h2 nr to a month

--- Scripts/calendar.js
+++ Scripts/calendar.js
@@ -1,5 +1,15 @@
 
 
+function loadPage() {
+
+    getMonthName()
+
+}
+
+
+
+
+
 const currentDate = new Date()
 document.getElementById("calendar").innerHTML = currentDate.getMonth() + 1;
 let numberOfDaysInMonth = new Date(currentDate.getFullYear(), currentDate.getMonth(), 0).getDate()
@@ -7,12 +17,49 @@ let numberOfDaysInMonth = new Date(currentDate.getFullYear(), currentDate.getMon
 console.log(numberOfDaysInMonth)
 
 let dateContainer = document.getElementById("dateContainer")
+let dayInWeek = new Date(currentDate.getFullYear(), 0 + 1, 1).getDay()
+
+for (let i = 0; i <= dayInWeek; i++) {
+    let dateBox = document.createElement("div")
+    
+    dateBox.classlist = 'days'
+    dateContainer.appendChild(dateBox)
+ 
+
+}
 
 for (let i = 1; i <= numberOfDaysInMonth; i++) {
-    console.log("add box")
     let dateBox = document.createElement("div")
     dateBox.innerText = i
     dateContainer.appendChild(dateBox)
+
+ 
+
 }
 
+
+
+
+function getMonthName() {
+    console.log('hej')
+    const d = new Date();
+    const month = new Array(11);
+    month[0] = "January";
+    month[1] = "February";
+    month[2] = "March";
+    month[3] = "April";
+    month[4] = "May";
+    month[5] = "June";
+    month[6] = "July";
+    month[7] = "August";
+    month[8] = "September";
+    month[9] = "October";
+    month[10] = "November";
+    month[11] = "December";
+    
+    const n = month[d.getMonth() + 1];
+    document.getElementById("calendar").innerText = n;
+    }
+
+    loadPage()
 
