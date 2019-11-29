404439286dc1ac79ec2704ec333bb50e147b7ba0
Mamta Srinivas <mamtasrinivassp@gmail.com>
2019-11-29 10:39:58 +0100

    Ready for review1

--- Scripts/todo.js
+++ Scripts/todo.js
@@ -1,17 +1,23 @@
+
+
+
 $(document).ready(function () {
     addEventListener()
     restoreTaskList()
 })
 
 
+function addEventListener() {
+    $(":submit").click(getTask)
+}
+
+/**
+ * Restores the existing task list from Local Storage
+ */
 function restoreTaskList() {
-    let data = localStorage.getItem("task")
-    let data2 = localStorage.getItem("task-time")
-    let data3 = localStorage.getItem("task-date")
+    let data = localStorage.getItem("task-details") // how to store as on object
     if (data) {
         let restoreList = JSON.parse(data)
-        let restoreList1 = JSON.parse(data)
-        let restoreList2 = JSON.parse(data)
         console.log(restoreList)
         for (var i = 0; i < (restoreList.length); i++) {
             $("ul").append("<li>" + restoreList[i] + "</li>")
@@ -21,51 +27,43 @@ function restoreTaskList() {
     }
 }
 
-function addEventListener() {
-    $(":submit").click(addTask)
-}
 
-function addTask() {
-    let task = $(":text").val()
-    let taskDate = $("#taskDate").val() //Why cant I use type here ?
-    let taskTime = $("#taskTime").val() // Why cant i use type here ?
 
-    console.log(taskDate)
-    console.log(taskTime)
+function getTask() {
 
-    $("ul").append("<li>" + task + " " + taskTime + " " + taskDate + "</li>")
+    const taskItemsArray = $("form").serializeArray()
+    const taskItemsObj = {}
+
+    $(taskItemsArray).each(function (i, field) {
+        taskItemsObj[field.name] = field.value
+    })
+
+    const task = taskItemsObj.text
+    const taskDate = taskItemsObj.date
+    const taskTime = taskItemsObj.time
+
+    showTask(task, taskDate, taskTime)
 
     const listOfTasks = []
     $("ul li").each(function () { listOfTasks.push($(this).text()) })
 
-    var fields = {};
-    $("#theForm").find(":input").each(function () {
-        // The selector will match buttons; if you want to filter
-        // them out, check `this.tagName` and `this.type`; see
-        // below
-        fields[this.name] = $(this).val();
-    });
-    var taskItems = { fields: fields };
-
     console.log(listOfTasks)
-    saveTaskToLS("task", listOfTasks)
-    saveTaskToLS("task-time", taskTime)
-    saveTaskToLS("task-date", taskDate)
-
-    addPrefix()
-    addSuffix()
+    saveTaskToLS("task-details", listOfTasks)
 
     $(":text").val("")
 
-}
 
-function addPrefix() {
 
 }
 
-function addSuffix() {
+function showTask(task, taskDate, taskTime) {
+    $("ul").append("<li><input type='checkbox'> " + task + "<br/> time:" + taskTime + "<br/> date:" + taskDate + " </li>")
+}
+
+function addCheckbox() {
 
 }
+
 
 function removeTask() {
     getTaskFromLS()
@@ -83,3 +81,4 @@ function getTaskFromLS(key) {
 function clearTaskFromLS() {
     localStorage.clear()
 }
+
