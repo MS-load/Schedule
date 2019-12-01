3cee2aca9ac0f8e0120e0de9b7a5c544c427903c
Mamta Srinivas <mamtasrinivassp@gmail.com>
2019-12-01 19:27:40 +0100

    Task Appears on Page Reload

--- Scripts/todo.js
+++ Scripts/todo.js
@@ -10,15 +10,14 @@ $(document).ready(function () {
 })
 
 
-function restoreTaskList(){
-    let OldArray = JSON.parse(localStorage.getItem('task-details')); //get data from storage
-  if (OldArray !== null) { //if data exist (todos are in storage)
-    OldArray.forEach(function(){ //append each element into the dom
+function restoreTaskList() {
+    let OldArray = JSON.parse(localStorage.getItem('task-details'))
+    console.log(OldArray)
+    if (OldArray !== null) {
         showTask(OldArray)
-    })
-  } else { //if nothing exist in storage, keep todos array empty
-    OldArray = [];
-  }
+    } else {
+        OldArray = []
+    }
 }
 
 function getDefaultValues() {
@@ -39,7 +38,7 @@ function getTask() {
     const savedArray = getTaskFromLS('task-details', taskItemsObj)
 
     saveTaskToLS('task-details', savedArray)
-    
+
     showTask(savedArray)
 
     $(":text").val("")
@@ -62,27 +61,28 @@ function saveTaskToLS(key, value) {
     localStorage.setItem(key, JSON.stringify(value))
 }
 
-function showTask(taskItemsArray) {
-    //const taskItemsArray = JSON.parse(localStorage.getItem('task-details'))
-    const taskText = taskItemsArray[taskItemsArray.length - 1].text
-    const taskTime = taskItemsArray[taskItemsArray.length - 1].time
-    const taskDate = taskItemsArray[taskItemsArray.length - 1].date
-    const taskNumber = taskItemsArray[taskItemsArray.length - 1].number
+function showTask(arrayPassed) {
+    $("ul").empty()
+    for (i = 0; i < arrayPassed.length; i++) {
+        const taskText = arrayPassed[i].text
+        const taskTime = arrayPassed[i].time
+        const taskDate = arrayPassed[i].date
+        const taskNumber = arrayPassed[i].number
 
-    const trash = "<i class='" + taskNumber + " material-icons'>delete</i>"
+        const trash = "<i class='" + taskNumber + " material-icons'>delete</i>"
 
-    $("ul").append(
-        "<li><div><p>" + taskText + "</p>" + trash + "</div><div><p> time: " +
-        taskTime + "</p><p> date: " + taskDate + "</p></div></li >")
-    addScrollToList()
-    $("." + taskNumber).click(function () {
-        removeTaskFromLS(taskNumber)
-        removeTask()
-    })
+        $("ul").append(
+            "<li><div><p>" + taskText + "</p>" + trash + "</div><div><p> time: " +
+            taskTime + "</p><p> date: " + taskDate + "</p></div></li >")
+
+        addScrollToList()
+
+        $("." + taskNumber).click(function () {
+            removeTaskFromLS(taskNumber)
+            removeTask()
+        })
+    }
 }
-
-
-
 
 function addScrollToList() {
     $("ul").scrollTop($("ul")[0].scrollHeight)
