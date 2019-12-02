7c522266640acc7ec6e51c1168261c6178925f4f
Lisa <lisa.bengtsson@live.com>
2019-12-02 13:18:43 +0100

    correct startday of month working and code more structured

--- Scripts/calendar.js
+++ Scripts/calendar.js
@@ -1,60 +1,54 @@
-
+window.addEventListener('load', loadPage)
 
 function loadPage() {
-    getMonthName()
-}
-
-
-
-const currentDate = new Date()
-
-document.getElementById("calendar").innerHTML = currentDate.getMonth() + 1
-
-const numberOfDaysInMonth = new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 0).getDate()
-
-console.log(numberOfDaysInMonth)
-
-const dateContainer = document.getElementById("dateContainer")
-const dayInWeek = new Date(currentDate.getFullYear(), currentDate.getMonth(), 1).getDay()
-
-
-console.log(dayInWeek)
-
-/**
- * Gives the correct day on first day of month
- */
-for (let i = 1; i < dayInWeek; i++) {
-    let dateBox = document.createElement("div")
-
-    dateBox.classlist = 'days'
-    dateContainer.appendChild(dateBox)
-
+    const dateContainer = document.getElementById("dateContainer")
+    const d = new Date()
+    getMonthName(d)
+    getCorrectFirstDay(dateContainer, d)
+    getNrOfDaysInMonth(dateContainer, d)
 
 }
 
-/**
- * Creates divs to print out all days in month in
- */
-for (let i = 1; i <= numberOfDaysInMonth; i++) {
-    let dateBox = document.createElement("div")
-    dateBox.innerText = i
-    dateContainer.appendChild(dateBox)
+
+function getCorrectFirstDay(dateContainer, d) {
+    const dayInWeek = new Date(d.getFullYear(), d.getMonth(), 0).getDay()
+    console.log(dayInWeek)
+    /**
+     * Gives the correct day on first day of month
+     */
+    for (let i = 0; i < dayInWeek; i++) {
+        let emptyBox = document.createElement("div")
+        emptyBox.innerText = " "
+        dateContainer.appendChild(emptyBox)
+
+
+    }
+
 }
 
+function getNrOfDaysInMonth(dateContainer, d) {
+    const numberOfDaysInMonth = new Date(d.getFullYear(), d.getMonth() + 1, 0).getDate()
+    console.log(numberOfDaysInMonth)
 
+    /**
+     * Creates divs to print out all days in month in
+     */
+    for (let i = 1; i <= numberOfDaysInMonth; i++) {
+        let dateBox = document.createElement("div")
+        dateBox.innerText = i
+        dateContainer.appendChild(dateBox)
+    }
+
+}
 
 /**
  * Writes month name in h2 title instead of just a number
  */
-function getMonthName() {
-    const d = new Date()
+function getMonthName(d) {
     const month = ["January", "February", "March", "April", "May", "June", "July",
-    "August", "September", "October", "November", "December"]
+        "August", "September", "October", "November", "December"]
 
-    const n = month[d.getMonth()]
-    document.getElementById("calendar").innerText = n
+    const monthName = month[d.getMonth()]
+    document.getElementById("calendar").innerText = monthName
 }
-
-
-loadPage()
 
