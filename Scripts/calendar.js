c6ca8317d3c7e30b2ba27ba8111888054d6c6273
Lisa <lisa.bengtsson@live.com>
2019-12-03 20:48:17 +0100

    added some comments

--- Scripts/calendar.js
+++ Scripts/calendar.js
@@ -10,7 +10,9 @@ function loadPage() {
     setupEventListeners()
 }
 
-
+/**
+ * Starts to write the calendar for this month
+ */
 function initCalendarMonth() {
     const dateContainer = document.getElementById("dateContainer")
     
@@ -19,23 +21,36 @@ function initCalendarMonth() {
     getNrOfDaysInMonth(dateContainer, date)
 }
 
+/**
+ * Writes the calendar for next month 
+ */
 function onNextClicked() {
     clearDivs()
     date.setMonth(date.getMonth() + 1)
     initCalendarMonth()
 }
 
+/**
+ * Writes the calendar for previous month
+ */
 function onPreviousClicked() {
     clearDivs()
     date.setMonth(date.getMonth() - 1)
     initCalendarMonth()
 }
 
+/**
+ * When buttons are clicked functions are runned to rewrite calendar for new month
+ * @param {mouseEvent}
+ */
 function setupEventListeners() {
     document.getElementById("previous-button").addEventListener("click", onPreviousClicked)
     document.getElementById("next-button").addEventListener("click", onNextClicked)
 }
 
+/**
+ * Clears the calendar ready to be rewritten when buttons are clicked
+ */
 function clearDivs() {
     document.getElementById("dateContainer").innerHTML = " ";
 }
