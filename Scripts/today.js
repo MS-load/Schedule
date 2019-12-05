86302b3677c0c99cf31fecc59a8f52a05bfc4848
Ellinor Carlberg <ellinor.carlberg@medieinstitutet.se>
2019-12-05 13:07:38 +0100

    done with the change-Icon function

--- Scripts/today.js
+++ Scripts/today.js
@@ -3,7 +3,7 @@ window.addEventListener('load', loadSite)
 
 function loadSite() {
     uppdateCurrentTime()
-    console.log(new Date())
+    changeIcon()
 }
 
 
@@ -21,11 +21,51 @@ function uppdateCurrentTime() {
         const date = new Date()
         const monthName = getMonthName(date)
         const weekdayName = getWeekdayName(date)
-
+        
         dateContainer.innerHTML = date.getDate() + " " + monthName + " " + date.getFullYear()
         weekdayContainer.innerHTML = weekdayName
         timeContainer.innerHTML = date.getHours() + ":" + (date.getMinutes() < 10 ? '0' : '') + date.getMinutes() + ":" + (date.getSeconds() < 10 ? '0' : '') + date.getSeconds()
     }, 1000)
+
+    
+}
+
+
+/**
+ * Change the icon source depending on current month
+ * 
+ */
+function changeIcon() {
+const date = new Date()    
+const Month = date.getMonth()
+let src
+    if (Month === 0) {
+    src = "./media/0.svg"
+} else if (Month === 1) {
+    src = "./media/1.svg"
+} else if (Month === 2) {
+    src = "./media/2.svg"
+} else if (Month === 3) {
+    src = "./media/3.svg"
+} else if (Month === 4) {
+    src = "./media/4.svg"
+} else if (Month === 5) {
+    src = "./media/5.svg"
+} else if (Month === 6) {
+    src = "./media/6.svg"
+} else if (Month === 7) {
+    src = "./media/7.svg"
+} else if (Month === 8) {
+    src = "./media/8.svg"
+} else if (Month === 9) {
+    src = "./media/9.svg"
+} else if (Month === 10) {
+    src = "./media/10.svg"
+} else if (Month === 11) {
+    src = "./media/11.svg"
+}
+
+document.getElementsByClassName("season-icon")[0].src=src
 }
 
 
